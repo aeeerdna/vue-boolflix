2c7e0e1a871fa23f6f7a6b451822730bc073ddc3
Andrea Miglietta <m.andrea96@hotmail.it>
2022-10-04 12:53:36 +0200

    ricerca film e lingua inserimento

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,32 @@
 <template>
-  <div>ciao</div>
+  <div id="app">ciao</div>
 </template>
 
 <script>
-export default {};
+import axios from "axios";
+import { apiKey } from "@/env.js";
+export default {
+  name: "App",
+  mounted() {
+    this.queryApi("8 mile");
+  },
+  methods: {
+    queryApi(searchTitle) {
+      axios
+        .get(
+          `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${searchTitle}&language=it-IT`
+        )
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((error) => {
+          console.log(error.message);
+        });
+    },
+  },
+};
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+@import "~bootstrap/scss/bootstrap.scss";
+</style>
