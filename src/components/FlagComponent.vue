871ca536be01b7b9776fbe183d1fe748037f8e31
Andrea Miglietta <m.andrea96@hotmail.it>
2022-10-04 16:41:11 +0200

    file .vue bandiere

---
+++ src/components/FlagComponent.vue
@@ -0,0 +1,54 @@
+<template>
+  <img
+    class="languageFlag ms-2"
+    :src="getFlag(language)"
+    :alt="language"
+    @error="fixImageError($event)"
+  />
+</template>
+
+<script>
+export default {
+  name: "FlagComponent",
+  props: {
+    language: String,
+  },
+  methods: {
+    getFlag(language) {
+      switch (language) {
+        case "en": {
+          language = "gb";
+          break;
+        }
+
+        case "ja": {
+          language = "jp";
+          break;
+        }
+
+        case "da": {
+          language = "dk";
+          break;
+        }
+
+        case "cs": {
+          language = "cz";
+          break;
+        }
+      }
+
+      return `https://flagicons.lipis.dev/flags/1x1/${language}.svg`;
+    },
+
+    fixImageError(event) {
+      event.target.src = `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuboE0D1KX9nCaXspFfaDmec4xbX_P2SBXYw&usqp=CAU`;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.languageFlag {
+  max-width: 15px;
+}
+</style>
